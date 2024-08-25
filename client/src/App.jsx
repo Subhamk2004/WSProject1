e59b1bfcd660a027a01cf35a549982a0230972a8
Subham Kumar <subhamk1978@gmail.com>
2024-08-25 19:55:18 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,54 @@
+import { io } from 'socket.io-client'
+import { useEffect, useMemo, useState } from 'react'
+
 function App() {
+  // only doing onChaneg amd then submitting causes the socket to refresh and change the connection on every text change so to prevent this we will be using  useMemo instead of doing just below:
+
+  // const socket = io('http://localhost:3000')
+  const socket = useMemo(() => io('http://localhost:3000'), []);
+  const [message, setMessage] = useState('');
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketId] = useState("");
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to the server')
+    })
+    socket.on('onboard', (data) => {
+      console.log(data);
+    })
+
+    socket.on('recieve', (data) => {
+      console.log(data);
+    })
+
+    return () => {
+      socket.disconnect()
+    }
+    // this return will run when we will refresh our page 
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+    setMessage("");
+  }
 
   return (
     <div className='h-[100vh] w-[100vw] bg-black/90'>
-      <h1 className="text-white">Hello React</h1>
+      <div className='h-full w-full  flex flex-col items-center p-10'>
+        <h1 className="text-white">Hello User</h1>
+        <form onSubmit={handleSubmit}>
+          <input type='text' required
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
 
 export default App
+// https://github.com/meabhisingh/socket.io
+// visit the above github repo for more on how to make group chats must see
--- server/src/index.mjs
+++ server/src/index.mjs
@@ -5,15 +5,44 @@ import cors from 'cors';
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    credentials: true,
+}));
 app.use(express.json());
 
 const server = createServer(app);
-const io = new Server(server, {});
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5174', 'http://localhost:5173'],
+        credentials: true,
+    },
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => {
+
+// io is entire circuit
+io.on('connection',(socket) => {
+    console.log('User Connected with ID: ' + socket.id);
+    socket.emit('onboard', `Welcome to the server`)
+    socket.broadcast.emit('onboard', `This is broadcast ${socket.id} just joined`)
+    // in the broadcast the socket.id is the id of the user sending the message 
+    socket.on('message', (data) => {
+        console.log(data);
+        // io.emit('recieve', data); this will send message to the sender itself
+        // socket.broadcast.emit('recieve', data); this will send message to all user but not the sender itself
+        
+    })
+
+    socket.on('disconnect',() => {
+        console.log('User disconnected', socket.id);
+    })
+})
+
+
+
+server.listen(PORT,() => {
     console.log('Server running on port ', PORT);
 });
 
